8ba5cb5c1dd88da110448b280e490a9a6237c4a9
EossForos <volosonopulos@gmail.com>
2020-03-14 21:26:25 +0200

    User-form

--- halls/views.py
+++ halls/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Hall
+
 
 def home(request):
     return render(request, 'halls/home.html')
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+
+
+# def create_hall(request):
+#     if request.meyhod == 'POST':
+#         pass
+#     else:
+#         pass
+
+
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
--- hofvidz/urls.py
+++ hofvidz/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -8,4 +9,12 @@ from halls import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    #AUTH
+    path('signup', views.SignUp.as_view(), name='signup'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    #Hall
+    path('halloffame/create', views.CreateHall.as_view(), name='create_hall'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
