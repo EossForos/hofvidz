310e40c1aabfc5442f9efbd30364e0495bcd46d1
EossForos <volosonopulos@gmail.com>
2020-03-18 14:17:49 +0200

    Deploing Heroku

--- halls/views.py
+++ halls/views.py
@@ -9,20 +9,25 @@ from .forms import VideoForm, SearchForm
 import urllib
 import requests
 from django.forms.utils import ErrorList
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 YOUTUBE_API_KEY = 'AIzaSyAKnXmuaOVHCxS6eX5f8F1B-kL2MD5ujcE'
 
 
 def home(request):
-    return render(request, 'halls/home.html')
+    recent_halls = Hall.objects.all().order_by('-id')[:3]
+    popular_halls = [Hall.objects.get(pk=1), Hall.objects.get(pk=2), Hall.objects.get(pk=3)]
+    return render(request, 'halls/home.html', {'recent_halls': recent_halls, 'popular_halls': popular_halls})
 
 
-
+@login_required()
 def dashboard(request):
-    return render(request, 'halls/dashboard.html')
+    halls = Hall.objects.filter(user=request.user)
+    return render(request, 'halls/dashboard.html', {'halls': halls})
 
 
-
+@login_required()
 def add_video(request, pk):
     form = VideoForm()
     search_form = SearchForm()
@@ -60,7 +65,7 @@ def add_video(request, pk):
                    'hall':hall})
 
 
-
+@login_required()
 def video_search(request):
     search_form = SearchForm(request.GET)
     if search_form.is_valid():
@@ -71,16 +76,22 @@ def video_search(request):
 
 
 
-class DeleteVideo(generic.DeleteView):
+class DeleteVideo(LoginRequiredMixin, generic.DeleteView):
     model = Video
     template_name = 'halls/delete_video.html'
     success_url = reverse_lazy('dashboard')
+
+    def get_object(self):
+        video = super(DeleteVideo, self).get_object()
+        if not video.hall.user == self.request.user:
+            raise Http404
+        return video
 
 
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('dashboard')
     template_name = 'registration/signup.html'
 
     def form_valid(self, form):
@@ -92,15 +103,8 @@ class SignUp(generic.CreateView):
 
 
 
-# def create_hall(request):
-#     if request.method == 'POST':
-#         pass
-#     else:
-#         pass
 
-
-
-class CreateHall(generic.CreateView):
+class CreateHall(LoginRequiredMixin, generic.CreateView):
     model = Hall
     fields = ['title']
     template_name = 'halls/create_hall.html'
@@ -109,7 +113,7 @@ class CreateHall(generic.CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         super(CreateHall, self).form_valid(form)
-        return redirect('home')
+        return redirect('dashboard')
 
 
 
@@ -119,15 +123,27 @@ class DetailHall(generic.DetailView):
 
 
 
-class UpdateHall(generic.UpdateView):
+class UpdateHall(LoginRequiredMixin, generic.UpdateView):
     model = Hall
     template_name = 'halls/update_hall.html'
     fields = ['title']
     success_url = reverse_lazy('dashboard')
 
+    def get_object(self):
+        hall = super(UpdateHall, self).get_object()
+        if not hall.user == self.request.user:
+            raise Http404
+        return hall
 
 
-class DeleteHall(generic.DeleteView):
+
+class DeleteHall(LoginRequiredMixin, generic.DeleteView):
     model = Hall
     template_name = 'halls/delete_hall.html'
     success_url = reverse_lazy('dashboard')
+
+    def get_object(self):
+        hall = super(DeleteHall, self).get_object()
+        if not hall.user == self.request.user:
+            raise Http404
+        return hall
